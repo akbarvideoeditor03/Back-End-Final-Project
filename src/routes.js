9517237b137037ec463717baca4a8a50257f38a8
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2024-12-09 23:05:17 +0700

    Back to use AuthMiddle

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,15 @@ const pendidikanTerakhir = require('./api/routes/pendidikanTerakhir.route');
 const pelatihan = require('./api/routes/pelatihan.route');
 const keahlian = require('./api/routes/keahlian.route');
 const authRoute = require('./api/routes/auth.route');
+const AuthMiddle = require('./middleware/authentication');
 
-router.use('api/v1/user', usersRoute);
-router.use('api/v1/user', authRoute);
+router.use('/api/v1/user', usersRoute);
+router.use('/api/v1/user', authRoute);
 
-router.use('api/v1/pengalamankerja', pengalamanKerjaRoute);
-router.use('api/v1/prestasikerja', prestasiKerjaRoute);
-router.use('api/v1/pendidikanterakhir', pendidikanTerakhir);
-router.use('api/v1/pelatihan', pelatihan);
-router.use('api/v1/keahlian', keahlian);
+router.use('/api/v1/pengalamankerja', AuthMiddle, pengalamanKerjaRoute);
+router.use('/api/v1/prestasikerja', AuthMiddle, prestasiKerjaRoute);
+router.use('/api/v1/pendidikanterakhir', AuthMiddle, pendidikanTerakhir);
+router.use('/api/v1/pelatihan', AuthMiddle, pelatihan);
+router.use('/api/v1/keahlian', AuthMiddle, keahlian);
 
 module.exports = router;
