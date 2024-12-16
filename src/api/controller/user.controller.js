63ca77de94ece614dd31e79a6923b8090ef32a5c
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2024-12-16 08:40:55 +0700

    7.91 Changes - Password Roll Back

--- src/api/controller/user.controller.js
+++ src/api/controller/user.controller.js
@@ -80,7 +80,6 @@ exports.updateUsers = async (req, res, next) => {
             req.body.tentang,
             req.body.foto_profil,
             req.body.email,
-            req.body.password,
         );
 
         return res.status(200).json({
