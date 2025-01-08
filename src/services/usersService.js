87412f5e0857c2da9e64822ee11bbdf7a5a4440a
ahmadakbar03 <akbarvideoeditor03@gmail.com>
2025-01-08 21:54:23 +0700

    9.2 Changes - Fix 1 Pengurutan findAll

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,6 +1,7 @@
 const { User } = require('../models');
 const { response } = require('express');
 const { encrypt } = require('../utils/password.util');
+const { CLOUD_SDK_CLIENT_ID } = require('google-auth-library/build/src/auth/googleauth');
 
 class UserService {
     async getUsers({ page, limit } = { page: 1, limit: 10 }) {
@@ -9,7 +10,12 @@ class UserService {
         }
 
         const countUsers = await User.count()
-        const users = await User.findAll(queryParams);
+        const users = await User.findAll({
+            ...queryParams,
+            order: [['id', 'ASC']]
+        });
+
+        console.log(users);
 
         return {
             data: users,
